e3f48f0e83362c9bdf3608490e97a32e672b14c7
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2024-12-26 12:08:02 +0500

    Parse config file

---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// we kept the camel case so that it can be exported with the help of this package
+//run go get -u github.com/ilyakaznacheev/cleanenv  , we can do annotations with the help of this package
+
+type HTTPServer struct {
+	Addr string
+}
+type Config struct {
+	Env         string `yaml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string `yaml:"storage-path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+// config is parsed and stored in the struct here
+func MustLoad() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+		configPath = *flags //dereferencing the flag
+
+		if configPath == "" {
+			log.Fatal("Config path is not set ")
+		}
+	}
+
+	//stat returns the file information
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config path does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	//first paraemter is configpath and second parameter is the structure in which it will be read
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		//printing string message of error
+		log.Fatalf("Config path does not exist: %s", err.Error())
+	}
+
+	//return address
+	return &cfg
+}
